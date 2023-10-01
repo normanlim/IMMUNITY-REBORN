a14e913cb16cc0ae165909c271a771d46ad8e491
Delaine <delainewtan@gmail.com>
2023-09-30 21:40:20 -0700

    Add magic and melee enemies with corresponding projectile types, and shield controls for the 3 types of shields.

--- ImmunityReborn/Assets/ProjectileCollision.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bb3fbe653ca8f1e5b432a64a779dcb6d6a605c37d711f2147142e1e53eeff4fc
-size 518
---
+++ ImmunityReborn/Assets/Scripts/ShieldTypeCollision.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6768bf040b9bd1d99128f4661575508eb31fe3f60b78f5f5fe587be071ab2c44
+size 286
