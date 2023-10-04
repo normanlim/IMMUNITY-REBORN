c51365a910b94ae9277dbc8ab79a1dfa0005f6d7
chrisyandev <czyanhome@hotmail.com>
2023-10-04 15:25:28 -0700

    apply gravity to player via script, change to attacking state when attack key pressed (only animation plays currently)

---
+++ ImmunityReborn/Assets/_TeamAmnesia/Scripts/Controls/ForceReceiver.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cd8a900d85d48c327060abed8cf73ab2c94e1bb3f790324e6fc9445af619c4b0
+size 653
--- ImmunityReborn/Assets/_TeamAmnesia/Scripts/State Machine/Player/PlayerStateMachine.cs
+++ ImmunityReborn/Assets/_TeamAmnesia/Scripts/State Machine/Player/PlayerStateMachine.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4969bf9a8828d3f7d28335dac8f50916ce13ad0c6b29087b434d26d76618fefd
-size 910
+oid sha256:96f33c5ff56ac07e1332ccb6d229311d715519db221076b8a6b48cd36c8b28f4
+size 1079
