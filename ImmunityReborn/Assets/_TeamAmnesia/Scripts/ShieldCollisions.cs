71c4d8e5afcdcffb5a3f391e339121226ccd8c19
Delaine <delainewtan@gmail.com>
2023-10-09 14:24:10 -0700

    Add attacks blocked counter for each shield.

---
+++ ImmunityReborn/Assets/_TeamAmnesia/Scripts/ShieldCollisions.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:22611da0f5098f277b1096846109b77bbc7dc71015486f44066fbee23a9f46f8
+size 589
--- ImmunityReborn/Assets/_TeamAmnesia/Scripts/ShieldTypeCollision.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f73d4bfbc20f2853b3acdd406bda4382029677300aca7b2abed362b24f13fb9a
-size 489
