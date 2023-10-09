bb31bc6e08c704b91038b31106d345c0d852437e
Delaine <delainewtan@gmail.com>
2023-10-09 01:01:15 -0700

    Add logic for individual shield gauges decreasing while shields are active, increasing while inactive after a delay, and prevent shields from being used while the gauge is empty.

--- ImmunityReborn/Assets/_TeamAmnesia/Scripts/ShieldController.cs
+++ ImmunityReborn/Assets/_TeamAmnesia/Scripts/ShieldController.cs
@@ -2,20 +2,67 @@ using UnityEngine;
 
 public class ShieldController : MonoBehaviour
 {
-    public GameObject meleeShield;
-    public GameObject rangedShield;
-    public GameObject magicShield;
-    public int meleeAttacksBlocked = 0;
-    public int rangedAttacksBlocked = 0;
-    public int magicAttacksBlocked = 0;
+    [System.Serializable]
+    public class Shield
+    {
+        public GameObject shieldObject;
+        public float rechargeDelay = 5.0f;
+        public float gaugeRemaining = 100;
+        public float rechargeTimer = 0.0f;
+    }
+    public Shield meleeShield;
+    public Shield rangedShield;
+    public Shield magicShield;
 
     void Update()
     {
         // Check if only the left mouse button is down
-        meleeShield.SetActive(Input.GetMouseButton(0) && !Input.GetMouseButton(1));
+        meleeShield.shieldObject.SetActive(Input.GetMouseButton(0) && !Input.GetMouseButton(1) && meleeShield.gaugeRemaining > 0);
         // Check if only the right mouse button is down
-        rangedShield.SetActive(Input.GetMouseButton(1) && !Input.GetMouseButton(0));
+        rangedShield.shieldObject.SetActive(Input.GetMouseButton(1) && !Input.GetMouseButton(0) && rangedShield.gaugeRemaining > 0);
         // Check if both mouse buttons are down
-        magicShield.SetActive(Input.GetMouseButton(0) && Input.GetMouseButton(1));
+        magicShield.shieldObject.SetActive(Input.GetMouseButton(0) && Input.GetMouseButton(1) && magicShield.gaugeRemaining > 0);
+
+        // Recharge the gauges with delay
+        RechargeShield(meleeShield);
+        RechargeShield(rangedShield);
+        RechargeShield(magicShield);
     }
+
+    private void RechargeShield(Shield shield)
+    {
+        if (!shield.shieldObject.activeSelf)
+        {
+            if (shield.rechargeTimer <= 0.0f)
+            {
+                // Recharge the gauge only when the rechargeTimer reaches 0
+                if (shield.gaugeRemaining < 100)
+                {
+                    shield.gaugeRemaining += Time.deltaTime * 10;
+                    // Cap the gauge at 100 if it exceeds
+                    shield.gaugeRemaining = Mathf.Clamp(shield.gaugeRemaining, 0, 100);
+                }
+            }
+            else
+            {
+                // Decrease the recharge timer
+                shield.rechargeTimer -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            // Reset the recharge timer when the shield is active
+            shield.rechargeTimer = shield.rechargeDelay;
+
+            // Decrease the gauge when the shield is active
+            if (shield.gaugeRemaining > 0)
+            {
+                shield.gaugeRemaining -= Time.deltaTime * 10;
+                // Ensure the gauge doesn't go below 0
+                shield.gaugeRemaining = Mathf.Clamp(shield.gaugeRemaining, 0, 100);
+            }
+        }
+    }
+
+
 }
