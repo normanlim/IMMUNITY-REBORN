2b9c1e62e81469b76b0aeebb47d499e3d9dcf227
Delaine <delainewtan@gmail.com>
2023-10-09 14:24:10 -0700

    Add attacks blocked counter for each shield.

--- ImmunityReborn/Assets/_TeamAmnesia/Scripts/ShieldCollisions.cs
+++ ImmunityReborn/Assets/_TeamAmnesia/Scripts/ShieldCollisions.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 
-public class ProjectileCollision : MonoBehaviour
+public class ShieldCollisions : MonoBehaviour
 {
     public string typeTag;
+    public int numAttacksBlocked = 0;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -10,7 +11,11 @@ public class ProjectileCollision : MonoBehaviour
         // Debug shows that this trigger gets called once on click (hits the player I guess)
         // Then 3 more times as 3 bullets touch the shield (to see which projectile should get deleted)
 
-        if (other.gameObject.tag == typeTag )
+        if (other.gameObject.tag == typeTag)
+        {
             Destroy(other.gameObject);
+            numAttacksBlocked++;
+        }
+            
     }
 }
