359f7c48ae0a7202bcde7f70fccc52cd954390c5
Norman Lim <norman.lim2@hotmail.com>
2023-10-08 23:16:42 -0700

    [Norman] Implemented shield uptime/regen logic and respective UI elements

--- ImmunityReborn/Assets/_TeamAmnesia/Scripts/ShieldController.cs
+++ ImmunityReborn/Assets/_TeamAmnesia/Scripts/ShieldController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48d35aa993dfc66ff3f01c4edf46ad08b236c466ab39e47f7456409d75ba96e6
-size 618
+oid sha256:03173b7e0d702c26afe16e72e9931ef6e7d98a4ba35cf13bc96de660696084f0
+size 2513
