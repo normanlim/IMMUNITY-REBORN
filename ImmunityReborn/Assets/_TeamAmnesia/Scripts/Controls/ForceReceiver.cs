0392ff7afd0a4db3a2396aa0edbbaeb7a80d3de7
Norman Lim <norman.lim2@hotmail.com>
2023-10-14 23:17:31 -0700

    [Norman] Merge main script changes before perforce migrate

--- ImmunityReborn/Assets/_TeamAmnesia/Scripts/Controls/ForceReceiver.cs
+++ ImmunityReborn/Assets/_TeamAmnesia/Scripts/Controls/ForceReceiver.cs
@@ -6,10 +6,14 @@ public class ForceReceiver : MonoBehaviour
 {
     [SerializeField]
     private CharacterController characterController;
+    [SerializeField]
+    private float drag = 0.3f;
 
+    private Vector3 dampingVelocity;
+    private Vector3 impact;
     private float verticalVelocity;
 
-    public Vector3 VerticalMovement => Vector3.up * verticalVelocity;
+    public Vector3 Movement => impact + Vector3.up * verticalVelocity;
 
     private void Update()
     {
@@ -21,5 +25,12 @@ public class ForceReceiver : MonoBehaviour
         {
             verticalVelocity += Physics.gravity.y * Time.deltaTime; // simulates gravity
         }
+
+        impact = Vector3.SmoothDamp(impact, Vector3.zero, ref dampingVelocity, drag);
+    }
+
+    public void AddForce(Vector3 force)
+    {
+        impact += force;
     }
 }
--- ImmunityReborn/Assets/_TeamAmnesia/Scripts/State Machine/State.cs
+++ ImmunityReborn/Assets/_TeamAmnesia/Scripts/State Machine/State.cs
@@ -7,4 +7,27 @@ public abstract class State
     public abstract void Enter();
     public abstract void Tick(float deltaTime);
     public abstract void Exit();
+
+    /// <summary>
+    /// Gets the currently playing animation's duration played no matter how long the animation is.
+    /// </summary>
+    /// <returns>0.0f to 1.0f where 1.0f represents 100% of animation played</returns>
+    protected float GetPlayingAnimationTimeNormalized(Animator animator)
+    {
+        AnimatorStateInfo currentInfo = animator.GetCurrentAnimatorStateInfo(0);
+        AnimatorStateInfo nextInfo = animator.GetNextAnimatorStateInfo(0);
+
+        if (animator.IsInTransition(0) && nextInfo.IsTag("Attack"))
+        {
+            return nextInfo.normalizedTime;
+        }
+        else if (!animator.IsInTransition(0) && currentInfo.IsTag("Attack"))
+        {
+            return currentInfo.normalizedTime;
+        }
+        else
+        {
+            return 0.0f;
+        }
+    }
 }
