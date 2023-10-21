558780a957095637410bbf1ca991beb192650c00
Delaine <delainewtan@gmail.com>
2023-10-21 14:43:18 -0700

    Change enemy rotation to face player when they shoot.

--- ImmunityReborn/Assets/_TeamAmnesia/Scripts/EnemyShootProjectile.cs
+++ ImmunityReborn/Assets/_TeamAmnesia/Scripts/EnemyShootProjectile.cs
@@ -16,10 +16,15 @@ public class EnemyShootProjectile : MonoBehaviour
 
     private void ShootAtPlayer()
     {
+        // Calculate the direction from the enemy to the player
         Vector3 playerDirection = player.transform.position - transform.position;
+
+        // Rotate the enemy to face the player
+        transform.LookAt(player.transform);
+
+        // Create a projectile and make it move towards the player
         GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
-        projectile.GetComponent<Rigidbody>().velocity = playerDirection;
+        projectile.GetComponent<Rigidbody>().velocity = playerDirection.normalized * speed;
         Destroy(projectile, 9f);
-        Debug.Log("hi");
     }
 }
