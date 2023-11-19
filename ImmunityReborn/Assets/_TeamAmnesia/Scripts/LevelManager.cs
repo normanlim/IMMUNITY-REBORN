620a6af09203070b54ff8695b2fd1cc7cb093d5f
Delaine <delainewtan@gmail.com>
2023-11-18 20:00:08 -0800

    Add BGM music to levels!

--- ImmunityReborn/Assets/_TeamAmnesia/Scripts/LevelManager.cs
+++ ImmunityReborn/Assets/_TeamAmnesia/Scripts/LevelManager.cs
@@ -3,30 +3,60 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] List<GameObject> LevelList = new List<GameObject>();
+    [SerializeField] List<Level> LevelList = new List<Level>();
     [SerializeField] int CurrentLevelIndex;
+    private AudioSource CurrentBGMSource;
 
+    [System.Serializable]
+    public class Level
+    {
+        public GameObject LevelObject;
+        public AudioClip LevelBGM;   
+    }
+    
     void Start()
     {
         CurrentLevelIndex = 0;
+        CurrentBGMSource = gameObject.AddComponent<AudioSource>();
+        CurrentBGMSource.loop = true;
+        PlayBGM(CurrentLevelIndex);
+    }
+
+    public void PlayBGM(int index)
+    {
+        if (index >= 0 && index < LevelList.Count)
+        {
+            CurrentBGMSource.clip = LevelList[index].LevelBGM;
+            CurrentBGMSource.Play();
+        }
+        else
+        {
+            Debug.LogError("Invalid BGM index");
+        }
+    }
+
+    public void StopBGM()
+    {
+        CurrentBGMSource.Stop();
     }
 
     public void StartNextLevel()
     {
         CurrentLevelIndex++;
-        if (CurrentLevelIndex >= LevelList.Count)
+        if (CurrentLevelIndex >= 0 && CurrentLevelIndex >= LevelList.Count)
         {
-            Debug.Log("There are no more levels, nothing happens.");
+            Debug.Log("Invalid Level index");
             return;
         }
         for (int i = 0; i < LevelList.Count; i++)
         {
             if (i == CurrentLevelIndex)
             {
-                LevelList[i].SetActive(true);
+                LevelList[i].LevelObject.SetActive(true);
+                PlayBGM(i);
             } else
             {
-                LevelList[i].SetActive(false);
+                LevelList[i].LevelObject.SetActive(false);
             }
         }
     }
