a21c73aea8a007cece5fcdd860242c6f8f79c81d
Delaine <delainewtan@gmail.com>
2023-12-06 17:49:03 -0800

    Some menu bugfixes.

--- ImmunityReborn/Assets/_TeamAmnesia/Scripts/Menu.cs
+++ ImmunityReborn/Assets/_TeamAmnesia/Scripts/Menu.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] GameObject MenuPlaySND;
-    public void PlayGame()
-    {
-        Debug.Log("StartGame button clicked");
-        PlaySFX.PlayThenDestroy(MenuPlaySND, transform);
-        SceneManager.LoadScene("Betac");
-    }
-
     public void ExitGame()
     {
         Debug.Log("ExitGame button clicked");
