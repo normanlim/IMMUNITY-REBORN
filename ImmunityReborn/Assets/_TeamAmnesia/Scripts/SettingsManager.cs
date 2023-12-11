a31cd9e35377722c54dacebb3f73fb47813a7c32
Delaine <delainewtan@gmail.com>
2023-12-10 17:38:08 -0800

    Only play menu sounds if the actual UI element is enabled and active.

--- ImmunityReborn/Assets/_TeamAmnesia/Scripts/SettingsManager.cs
+++ ImmunityReborn/Assets/_TeamAmnesia/Scripts/SettingsManager.cs
@@ -7,12 +7,15 @@ public class SettingsManager : MonoBehaviour
     public Toggle softcoreToggle;
     public Dropdown difficultyDropdown;
     private int previousDifficultyIndex = -1;
+    public GUISoundManager soundManager;
 
     void Start()
     {
+
         difficultyDropdown.onValueChanged.AddListener( delegate { DifficultyChanged(); } );
         softcoreToggle.onValueChanged.AddListener( delegate { ToggleSoftcore(); });
         godModeToggle.onValueChanged.AddListener( delegate { ToggleGodMode(); } );
+        soundManager = FindFirstObjectByType<GUISoundManager>();
     }
 
     public void DifficultyChanged()
@@ -25,15 +28,21 @@ public class SettingsManager : MonoBehaviour
 
             previousDifficultyIndex = difficultyIndex;
         }
+        if (difficultyDropdown.isActiveAndEnabled)
+            soundManager.PlaySelectConfirmation();
     }
 
     public void ToggleGodMode()
     {
         PlayerPrefs.SetInt( "GodMode", godModeToggle.isOn ? 1 : 0 );
+        if (godModeToggle.isActiveAndEnabled)
+            soundManager.PlaySelectConfirmation();
     }
 
     public void ToggleSoftcore()
     {
         PlayerPrefs.SetInt("Softcore", softcoreToggle.isOn ? 1 : 0);
+        if (softcoreToggle.isActiveAndEnabled)
+            soundManager.PlaySelectConfirmation();
     }
 }
